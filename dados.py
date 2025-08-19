444136ece379fa3435b70a48a1a2799281717ef8
Ivan Massanares <ivanmassanares@gmail.com>
2025-08-19 07:44:35 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,8 @@
+CIDADES = {
+    "São Paulo": {"Rio de Janeiro": 430, "Belo Horizonte": 586},
+    "Rio de Janeiro": {"São Paulo": 430, "Belo Horizonte": 434},
+    "Belo Horizonte": {"São Paulo": 586, "Rio de Janeiro": 434, "Brasília": 716},
+    "Brasília": {"Belo Horizonte": 716},
+    "Curitiba": {"São Paulo": 408},
+    "Porto Alegre": {"Curitiba": 711}
+}
